2a46b0c6f7f2e002b6b680d2a0efb5dfda26b724
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-06-15 16:42:14 -0300

    8.6 sons e musica de fundo

--- jogo/explosao.js
+++ jogo/explosao.js
@@ -1,5 +1,10 @@
 // Arquivo: explosao.js
 
+var SOM_EXPLOSAO = new Audio();
+SOM_EXPLOSAO.src = 'snd/explosao.mp3';
+SOM_EXPLOSAO.volume = 0.4;
+SOM_EXPLOSAO.load();
+
 function Explosao(context, imagem, x, y) {
     this.context = context;
     this.imagem = imagem;
@@ -7,12 +12,18 @@ function Explosao(context, imagem, x, y) {
     this.spritesheet.intervalo = 75;
     this.x = x;
     this.y = y;
+
     var explosao = this;
+
     this.fimDaExplosao = null;
+
     this.spritesheet.fimDoCiclo = function() {
         explosao.animacao.excluirSprite(explosao);
         if (explosao.fimDaExplosao) explosao.fimDaExplosao();
     }
+
+    SOM_EXPLOSAO.currentTime = 0.0;
+    SOM_EXPLOSAO.play();
 }
 
 Explosao.prototype = {
--- jogo/tiro.js
+++ jogo/tiro.js
@@ -1,5 +1,10 @@
 // arquivo: tiro.js
 
+var SOM_TIRO = new Audio();
+SOM_TIRO.src = 'snd/tiro.mp3';
+SOM_TIRO.volume = 0.2;
+SOM_TIRO.load();
+
 function Tiro(context, nave) {
     this.context = context;
     this.nave = nave;
@@ -13,6 +18,9 @@ function Tiro(context, nave) {
 
     // Escolha sua cor!
     this.cor = 'yellow';
+
+    SOM_TIRO.currentTime = 0.0;
+    SOM_TIRO.play();
 }
 
 Tiro.prototype = {
