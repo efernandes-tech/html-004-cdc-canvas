fec2b8555d6010f142dae07666d7c9948cf5ddea
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-06-15 16:35:25 -0300

    8.5 pausando o jogo

--- jogo/animacao.js
+++ jogo/animacao.js
@@ -17,6 +17,7 @@ Animacao.prototype = {
         sprite.animacao = this;
     },
     ligar: function() {
+        this.ultimoCiclo = 0;
         this.ligado = true;
         this.proximoFrame();
     },
--- jogo/teclado.js
+++ jogo/teclado.js
@@ -1,10 +1,12 @@
 // arquivo: teclado.js
+
 // Códigos de teclas - aqui vão todos os que forem necessários.
 var SETA_ESQUERDA = 37;
 var SETA_ACIMA = 38;
 var SETA_DIREITA = 39;
 var SETA_ABAIXO = 40;
 var ESPACO = 32;
+var ENTER = 13;
 
 function Teclado(elemento) {
     this.elemento = elemento;
